ba466fa333470af42b4d3b89fc0065ab7f829e29
박병천 <genius178896@gmail.com>
2024-11-22 15:29:24 +0900

    Refactor : 불필요한 중복 코드 제거
    - 음식을 나열해주는 로직 메서드가 중복되어 정리

--- src/kiosk/lv4/Kiosk.java
+++ src/kiosk/lv4/Kiosk.java
@@ -33,12 +33,16 @@ public class Kiosk {
             switch (categoryChoiceNo) {
                 case 1:
                     System.out.println("[ BURGERS MENU ]");
+                    /**
+                     *  BURGER 카테고리를 선택 시 BURGER 메뉴들을 보여주는 메서드
+                     *  메뉴들이 통합적으로 들어있는 리스트에서 특정한(BURGER) 타입만 꺼내도록 만들었습니다.
+                     */
                     List<Burger> burgers =menuItems.stream()
                             .filter(menu -> menu instanceof Burger)
                             .map(menu -> (Burger)menu)
                             .toList();
-                    printBurgerMenu(burgers);
-                    selectMenuOption(scanner, burgers);
+                    printOptionMenus(burgers);
+                    printMenuItemDetails(scanner, burgers);
 
                     break;
 
@@ -48,8 +52,8 @@ public class Kiosk {
                             .filter(menu -> menu instanceof Drink)
                             .map(menu -> (Drink)menu)
                             .toList();
-                    printDrinkMenu(drinks);
-                    selectMenuOption(scanner,drinks);
+                    printOptionMenus(drinks);
+                    printMenuItemDetails(scanner,drinks);
 
                     break;
                 case 3:
@@ -58,8 +62,8 @@ public class Kiosk {
                             .filter(menu -> menu instanceof Dessert)
                             .map(menu -> (Dessert)menu)
                             .toList();
-                    printDessertMenu(desserts);
-                    selectMenuOption(scanner,desserts);
+                    printOptionMenus(desserts);
+                    printMenuItemDetails(scanner,desserts);
                     break;
                 case 0:
                     System.out.println("시스템을 종료합니다.");
@@ -69,7 +73,12 @@ public class Kiosk {
         }
     }
 
-    private void selectMenuOption(Scanner scanner, List<? extends MenuItem> menuItems) {
+    /**
+     * 구체적인(ex.CheeseBurger)를 선택하면 이름, 가격, 설명을 보여주는 메서드
+     * @param scanner 를 매개변수로 받아서 음식들을 고를 수 있게 했다.
+     * @param menuItems 는 MenuItem의 자식들만 받을 수 있게 타입 안정성을 위해 사용
+     */
+    private void printMenuItemDetails(Scanner scanner, List<? extends MenuItem> menuItems) {
         System.out.println("0. 종료 "+ DELIMITER + " 종료");
         System.out.print("번호를 선택해주세요. : ");
         int Choice = scanner.nextInt();
@@ -80,9 +89,13 @@ public class Kiosk {
             return;
         }
 
-        // switch문을 사용 안하고 if-else 문을 사용한 이유 : System.out.println 부분 코드가 너무 중복되어서 입니다.
+        /**
+         * 선택한 메뉴의 상세정보를 출력해주는 조건문
+         * switch문을 사용 안하고 if-else 문을 사용한 이유 : System.out.println 부분 코드가 너무 중복되어서.
+         * menuItems.get(Choice - 1)을 변수명으로 뺀 이유 : 코드가 길어져서 가독성이 떨어지고 중복되는거라 변수로 따로 뺏습니다.
+         */
         if (Choice > 0 && Choice <= menuItems.size()) {
-            MenuItem menuItem = menuItems.get(Choice - 1); //코드가 길어져서 가독성이 떨어지고 중복되는거라 변수로 따로 빼줬습니다.
+            MenuItem menuItem = menuItems.get(Choice - 1);
             System.out.println("선택하신 메뉴 : [이름: " + menuItem.getName() +", 가격: " + menuItem.getPrice()
                     +", 설명: " + menuItem.getExplain()+"]");
         } else {
@@ -90,28 +103,15 @@ public class Kiosk {
         }
     }
 
+
     /**
-     *  BURGER 카테고리를 선택 시 BURGER 메뉴들을 보여주는 메서드
-     *  메뉴들이 통합적으로 들어있는 리스트에서 특정한(BURGER) 타입만 꺼내도록 만들었습니다.
+     * 카테고리에서 선택된 음시들을 나열해주는 메서드
+     * @param menuItems 의 각각의 자식들을 파라미터로 받아서 정보를 출력
      */
-    private void printBurgerMenu(List<Burger> burgers) {
-        for (int i = 0; i < burgers.size(); i++) {
-            System.out.println((i+1)+ "." + burgers.get(i).getName() + DELIMITER + burgers.get(i).getPrice()
-                    + DELIMITER + burgers.get(i).getExplain());
-        }
-    }
-
-    private void printDrinkMenu(List<Drink> drinks) {
-        for (int i = 0; i < drinks.size(); i++) {
-            System.out.println((i+1)+ "." + drinks.get(i).getName() + DELIMITER + drinks.get(i).getPrice()
-                    + DELIMITER + drinks.get(i).getExplain());
-        }
-    }
-
-    private void printDessertMenu(List<Dessert> desserts) {
-        for (int i = 0; i < desserts.size(); i++) {
-            System.out.println((i+1)+ "." + desserts.get(i).getName() + DELIMITER + desserts.get(i).getPrice()
-                    + DELIMITER + desserts.get(i).getExplain());
+    private void printOptionMenus(List<? extends MenuItem> menuItems) {
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println((i+1)+ "." + menuItems.get(i).getName() + DELIMITER + menuItems.get(i).getPrice()
+                    + DELIMITER + menuItems.get(i).getExplain());
         }
     }
 
