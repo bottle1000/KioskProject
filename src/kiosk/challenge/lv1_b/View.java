c644342fb7fb99f62010c717f1e3b97456b734ad
박병천 <genius178896@gmail.com>
2024-11-27 20:27:55 +0900

    chore: 파일추가

--- src/kiosk/challenge/lv1_b/View.java
+++ src/kiosk/challenge/lv1_b/View.java
@@ -1,7 +1,4 @@
-package kiosk.challenge.lv2_b;
-
-import kiosk.challenge.lv2_b.cart.CartItem;
-import kiosk.challenge.lv2_b.cart.CartManager;
+package kiosk.challenge.lv1_b;
 
 import java.util.List;
 import java.util.Scanner;
@@ -30,7 +27,6 @@ public class View {
         System.out.println("0. 종료   " + DELIMITER + "종료");
     }
 
-// ----
 
     /**
      * 메뉴에서 번호를 선택하여 음식들을 순번대로 보여주는 메서드
@@ -50,36 +46,45 @@ public class View {
         System.out.println("0. 뒤로가기");
     }
 
-    // 메뉴 선택 및 장바구니 추가 관련 처리
-    public void printSelectItem(List<Menu> menuList, int mainChoice, int subChoice) {
-        // 메인 메뉴와 서브 메뉴 선택
-        Menu selectMenu = menuList.get(mainChoice - 1);
+    public void printSelectedItem(List<Menu> menuList, int menuListIndex, int itemListIndex) {
+        MenuItem menuItem = menuList.get(menuListIndex - 1).getMenuItemList().get(itemListIndex - 1);
+        System.out.println("선택한 메뉴 : " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplain());
+    }
 
-        // 서브 메뉴 아이템 유효성 검사
-        if (subChoice > 0 && subChoice <= selectMenu.getMenuItemList().size()) {
-            MenuItem menuItem = selectMenu.getMenuItemList().get(subChoice - 1);
-            printSelectItem(menuItem);
-            printConfirmOrder();  // 추가 확인 여부
-        } else {
-            System.out.println("잘못된 선택입니다! 다시 시도해주세요.");
+
+    public MenuItem printAskAddToCart(List<Menu> menuList, int menuListIndex, int itemListIndex) {
+        MenuItem menuItem = menuList.get(menuListIndex - 1).getMenuItemList().get(itemListIndex - 1);
+        System.out.println("\"" + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplain() + "\"");
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.println("1. 확인     2. 취소");
+        return menuItem;
+    }
+
+    public void printConfirmAddToCart(MenuItem menuItem) {
+        String itemName = menuItem.getName();
+        System.out.println(itemName + "가(이) 장바구니에 추가되었습니다.");
+    }
+
+    public void displayChoiceMenus() {
+        System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
+    }
+
+    public void printOrderMenu(List<Menu> menuList) {
+        OrderMenu[] values = OrderMenu.values();
+        for (int i = 0; i < values.length; i++) {
+            System.out.println((menuList.size() + (i+1))+ ". " + values[i] + "     | " + values[i].getDescription());
         }
     }
-    // 메뉴 항목 선택 후 추가 여부 확인 출력
-    public void printSelectItem(MenuItem menuItem) {
-        System.out.println("\"" + menuItem.getName() + "\"");
-        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
-    }
 
-    // 확인 및 취소 선택 출력
-    public void printConfirmOrder() {
-        System.out.println("1. 확인     2. 취소");
-    }
-
-    public void printCart(List<CartItem> cartItemList){
+    public void printAskOrder() {
         System.out.println("아래와 같이 주문 하시겠습니까?");
+    }
+
+    public void printCartList(CartManager cartManager) {
         System.out.println("[ Orders ]");
+        List<CartItem> cartItemList = cartManager.getCartItemList();
         for (CartItem cartItem : cartItemList) {
-            System.out.println(cartItem.getItemName() + "  | " + cartItem.getPrice() + " | " + cartItem.getQuantity());
+            System.out.println(cartItem.getItemName() + "  | W " + cartItem.getPrice() + " | " + cartItem.getQuantity() );
         }
     }
 
@@ -88,28 +93,12 @@ public class View {
         System.out.println("W " + cartManager.calculateTotalPrice());
     }
 
-    public void printMenus(List<Menu> menuList) {
-        System.out.println("아래 메뉴판을 보시고 메뉴를 골라주세요.");
-        printMainMenu(menuList);
-        System.out.println("[ ORDER MENU ]");
-        OrderMenu[] values = OrderMenu.values();
-        for (int i = 0; i < values.length; i++) {
-            System.out.println((menuList.size() + (i+1))+ ". " + values[i] + "     | " + values[i].getDescription());
-        }
+    public void printChoiceConfirmOrder() {
+        System.out.println("1. 주문     2. 메뉴판");
     }
 
-
-    public void showOrderOptions(CartManager cartManager) {
-        System.out.println("1. 주문        2. 메뉴판");
-        int userInput = getUserInput();
-        switch (userInput) {
-            case 1:
-                System.out.println("주문이 완료되었습니다. 금액은 W " + cartManager.calculateTotalPrice() + "입니다.");
-                cartManager.clearCart();
-                break;
-            case 2:
-                System.out.println("메뉴를 추가하시겠습니까?");
-        }
+    public void printCompleteOrderAndTotalPrice(CartManager cartManager) {
+        System.out.println("주문이 완료되었습니다. 금액은 W " + cartManager.calculateTotalPrice() + " 입니다.");
     }
 
 }
